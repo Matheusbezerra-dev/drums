fa902c9b7a4d5bbaaf9dfacb0735b52d15e4d22f
Matheus Bezerra <mabezerra2@gmail.com>
2022-06-15 16:42:38 -0400

    criando as divs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
-function createDiv{
-  let arrayOne = ['Q','W','E','A','S','D','Z','X','C'];
-  let arrayTwo = ['q','w','e','a','s','d','z','x','c'];
+function createAudios() {
+  const arrayThre = ['q','w','e','a','s','d','z','x','c'];
+  for (let index = 0; index < arraythre.length; index += 1) {
+    const createAudio = document.createElement('audio');
+    document.body.appendChild(createAudio);
+    createAudio.setAttribute('id', `s_key`)      
+  }
+}
+
+function createDivs() { 
+  const arrayOne = ['Q','W','E','A','S','D','Z','X','C'];
+  const arrayTwo = ['q','w','e','a','s','d','z','x','c'];
+  const createDiv1 = document.createElement('div');
+  document.getElementById('container').appendChild(createDiv1);
+  createDiv1.setAttribute('id', 'keys');
+  for (let i = 0; i < arrayOne.length; i += 1) {
+    const createDiv = document.createElement('div')
+    document.getElementById('keys').appendChild(createDiv);
+    createDiv.setAttribute('id', `key${arrayTwo[i]}`) ;
+    createDiv.innerText = arrayOne[i];
+  }
+  createAudios()
 }
 
 function tagH1() {
@@ -18,6 +37,7 @@ function createMainAndHeader() {
   document.body.appendChild(createMain);
   createMain.setAttribute('id', 'container');
   tagH1()
+  createDivs()
 }
 
 window.onload = () => {
